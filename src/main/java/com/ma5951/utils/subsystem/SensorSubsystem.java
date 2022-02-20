618afc8027e7b0d051cf7d5d3946d7e164c6cef2
“yoavkaplan123git config --global user.email “yoav110702@gmail.com <yoav110702@gmail.com>
2022-02-20 22:44:41 +0200

    Update SensorSubsystem.java
    fixes in ma utils

--- src/main/java/com/ma5951/utils/subsystem/SensorSubsystem.java
+++ src/main/java/com/ma5951/utils/subsystem/SensorSubsystem.java
@@ -5,5 +5,7 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 public interface SensorSubsystem extends Subsystem {
     public boolean canMove();
 
-    public void reset();
+    default void reset(){
+        return;
+    }
 }
