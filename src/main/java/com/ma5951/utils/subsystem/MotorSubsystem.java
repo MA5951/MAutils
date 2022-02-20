17789ebdbedcbd8bf75a46beef17f7b68e4cdf54
Konata <42537566+konata-chan404@users.noreply.github.com>
2022-02-20 20:59:32 +0200

    Update MotorSubsystem.java
    update the interface

--- src/main/java/com/ma5951/utils/subsystem/MotorSubsystem.java
+++ src/main/java/com/ma5951/utils/subsystem/MotorSubsystem.java
@@ -1,8 +1,6 @@
 package com.ma5951.utils.subsystem;
 
-import edu.wpi.first.wpilibj2.command.Subsystem;
-
-public interface MotorSubsystem extends Subsystem {
+public interface MotorSubsystem extends SensorSubsystem {
     public void setVoltage(double voltage);
 
     default void setPower(double power) {
