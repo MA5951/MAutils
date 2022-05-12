2710ed65f44ceb84505ed53689af60f6a895a9d8
Konata <42537566+konata-chan404@users.noreply.github.com>
2022-05-12 19:43:09 +0300

    idk

---
+++ src/main/java/com/ma5951/utils/led/AddressableLEDPattern.java
@@ -0,0 +1,16 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.ma5951.utils.led;
+
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+
+/** Add your docs here. */
+public interface AdressableLEDPattern {
+    public void setLEDs(AddressableLEDBuffer buffer);
+
+    default public boolean isAnimated() {
+        return false;
+    }
+}
---
+++ src/main/java/com/ma5951/utils/led/SolidColorPattern.java
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.ma5951.utils.led;
+
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.util.Color;
+
+/** Add your docs here. */
+public class SolidColorPattern implements AddressableLEDPattern {
+
+    Color color;
+
+    public SolidColorPattern(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public void setLEDs(AddressableLEDBuffer buffer) {
+        for (int i = 0; i < buffer.getLength(); i++) {
+            buffer.setLED(i, color);
+        }
+    }
+
+}
