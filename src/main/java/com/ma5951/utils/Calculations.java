20493aa9affa42c2240a1a9085c6b5139b49b091
EilonK05 <eilonlon.klein@gmail.com>
2022-07-07 14:17:59 +0300

    Driver Station and Calculations Javadocs done

--- src/main/java/com/ma5951/utils/Calculations.java
+++ src/main/java/com/ma5951/utils/Calculations.java
@@ -2,6 +2,10 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/**
+ * Making calculations
+ */
+
 package com.ma5951.utils;
 
 import edu.wpi.first.math.MathUtil;
@@ -9,25 +13,66 @@ import edu.wpi.first.math.MathUtil;
 public class Calculations {
     private final static double K_ELECTRON_CHARGE = -1.6e-19;
 
+    /**
+     * Converting from RPM to linear speed
+     * @param RPM The RPM to convert
+     * @param gear The gear that attached to the motor
+     * @return The linear speed
+     */
+
     public static double fromRPMToLinearSpeed(double RPM, double gear) {
         return (RPM / 10) * gear;
     }
+
+    /**
+     * Converting from linear speed to RPM
+     * @param linearSpeed The linear speed to convert
+     * @param gear The gear that attached to the motor
+     * @return The RPM
+     */
 
     public static double fromLinearSpeedToRPM(double linearSpeed, double gear) {
         return (linearSpeed / gear) * 10;
     }
 
+    /**
+     * Converting from encoder to RPM
+     * @param Rate The econder value to convert
+     * @param TPR The encoder's ticks per round
+     * @return The RPM
+     */
+
     public static double toRPMFromEncoder(double Rate, int TPR) {
         return (Rate / TPR) / 60;
     }
+
+    /**
+     * Convert from encoder that connected to Talon to RPM
+     * @param Rate The encoder value to convert
+     * @param TPR The ecnoder's ticks per round
+     * @return The RPM
+     */
 
     public static double toRPMFromEncoderConnectToTalon(double Rate, int TPR) {
         return ((Rate * 10) / TPR) / 60;
     }
 
+    /**
+     * Convert from force to voltage
+     * @param force The force to convert
+     * @return The Voltage
+     */
+
     public static double ForceToVoltage(double force) {
         return MathUtil.clamp(force / K_ELECTRON_CHARGE, 12, -12);
     }
+
+    /**
+     * Convert from RPM to voltage
+     * @param RPM The RPM to convert
+     * @param maxRPM The motor's maximum RPM
+     * @return The voltage
+     */
 
     public static double RPMToVoltage(double RPM, double maxRPM) {
         return (RPM / maxRPM) * 12;
--- src/main/java/com/ma5951/utils/DriverStation.java
+++ src/main/java/com/ma5951/utils/DriverStation.java
@@ -1,7 +1,7 @@
 package com.ma5951.utils;
 
 /**
- * Add your docs here.
+ * Adding utilities for the driver station
  */
 public class DriverStation {
     private String alliance = "";
@@ -9,38 +9,85 @@ public class DriverStation {
     private Shuffleboard driverStationShuffleboard = new Shuffleboard("SmartDashboard");
     private static DriverStation driverStation;
 
+    /**
+     * Creating a new Driver Station object
+     */
+
     private DriverStation() {
         alliance = edu.wpi.first.wpilibj.DriverStation.getAlliance().toString();
         location = edu.wpi.first.wpilibj.DriverStation.getLocation();
     }
 
+    /**
+     * Printing error to the Driver Station
+     * @param error Error to print
+     * @param printTrace To print the stack trace or not
+     */
+
     public static void printError(String error, boolean printTrace) {
         edu.wpi.first.wpilibj.DriverStation.reportError(error, printTrace);
     }
+
+    /**
+     * Printing warning to the Driver Station
+     * @param error Error to print
+     * @param printTrace To print the stack trace or not
+     */
 
     public static void printWarning(String error, boolean printTrace) {
         edu.wpi.first.wpilibj.DriverStation.reportWarning(error, printTrace);
     }
 
+    /**
+     * Getting the game spacific message from the FMS
+     * @return The game spacific message
+     */
+
     public static String getGameSpecificMessage() {
         return edu.wpi.first.wpilibj.DriverStation.getGameSpecificMessage();
     }
+
+    /**
+     * Check if Telleop is enabled
+     * @return if Telleop is enabled
+     */
 
     public static boolean isOperatorControlEnabled() {
         return edu.wpi.first.wpilibj.DriverStation.isTeleopEnabled();
     }
 
+    /**
+     * Check if the FMS is attached
+     * @return if the FMS is attached
+     */
+
     public static boolean isFMSAttached() {
         return edu.wpi.first.wpilibj.DriverStation.isFMSAttached();
     }
 
+    /**
+     * Check if the Driver Station is attached
+     * @return if the Driver Station is attached
+     */
+
     public static boolean isDSAttached() {
         return edu.wpi.first.wpilibj.DriverStation.isDSAttached();
     }
+    
+    /**
+     * Getting if Autonomous is enabled
+     * @return if Autonomous is enabled
+     */
 
     public static boolean isAutonomousEnabled() {
         return edu.wpi.first.wpilibj.DriverStation.isAutonomousEnabled();
     }
+
+    /**
+     * Check if there is a joystick connected to specific port
+     * @param Port The port to check
+     * @return if there is a joystick that connected to the port
+     */
 
     public static boolean isJoystickConnected(int Port) {
         return edu.wpi.first.wpilibj.DriverStation.isJoystickConnected(Port);
@@ -54,6 +101,11 @@ public class DriverStation {
     public void periodic() {
         printValues();
     }
+
+    /**
+     * Singletone
+     * @return the Driver Station instance
+     */
 
     public static DriverStation getInstance() {
         if (driverStation == null) {
