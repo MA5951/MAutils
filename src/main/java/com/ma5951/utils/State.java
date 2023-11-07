764abc76eb71bf312ec081ffa5be7338935cc311
GalDotan <gldotan@gmail.com>
2023-11-07 23:35:42 +0200

    weip

---
+++ src/main/java/com/ma5951/utils/State.java
@@ -0,0 +1,34 @@
+package com.ma5951.utils;
+
+import java.util.concurrent.locks.Condition;
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class State {
+    private Supplier<Boolean> condition;
+    private Command command;
+    
+    private int commandState;
+
+    public State(Supplier<Boolean> Condition , Command Command ) {
+        condition = Condition;
+        command = Command;
+        commandState = 0;
+        
+    }
+
+    public void runState() {
+       
+       if (condition.get() && commandState == 0) {
+            command.initialize();
+            commandState++;       
+        } else if (condition.get() && commandState == 1) {
+            command.execute();
+        } else if ()
+        
+        
+    }
+
+
+}
