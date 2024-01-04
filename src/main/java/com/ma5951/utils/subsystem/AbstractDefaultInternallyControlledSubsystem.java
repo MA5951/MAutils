2e6f2537342932585731ec6aa994b159efbc97ae
noya-isaiah <noya2110@gmail.com>
2024-01-04 13:23:36 +0200

    fix AbstractDefultInternallyControlledSubsystem

--- src/main/java/com/ma5951/utils/subsystem/AbstractDefaultInternallyControlledSubsystem.java
+++ src/main/java/com/ma5951/utils/subsystem/AbstractDefaultInternallyControlledSubsystem.java
@@ -4,12 +4,35 @@
 
 package com.ma5951.utils.subsystem;
 
-/** Add your docs here. */
+import java.util.function.Supplier;
+
+import com.ma5951.utils.MAShuffleboard;
+import com.ma5951.utils.MAShuffleboard.pidControllerGainSupplier;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
 public abstract class AbstractDefaultInternallyControlledSubsystem 
-implements DefaultInternallyControlledSubsystem{
+    extends SubsystemBase implements DefaultInternallyControlledSubsystem{
 
     private double setPoint;
-    private double tolerance;
+    private double tolorance;
+    protected MAShuffleboard shuffleboard;
+    protected pidControllerGainSupplier pidGainSupplier;
+    private Supplier<Double> getMeasurement;
+
+    public AbstractDefaultInternallyControlledSubsystem(
+        double tolorance, String tabName, Supplier<Double> getMeasurment){
+        this.tolorance = tolorance;
+        shuffleboard = new MAShuffleboard(tabName);
+        pidGainSupplier = shuffleboard.getPidControllerGainSupplier();
+        this.getMeasurement = getMeasurment;
+    }
+
+    public boolean atPoint(){
+        return Math.abs(this.getMeasurement.get() - setPoint)
+         <= this.tolorance;
+    }
 
     public double getSetPoint(){
         return this.setPoint;
@@ -19,7 +42,12 @@ implements DefaultInternallyControlledSubsystem{
         this.setPoint = setPoint;
     }
 
-    public boolean atPoint(){
-        return Math.abs(getMeasurement() - setPoint) <= this.tolerance;
-    }
+    public void periodic() {
+        shuffleboard.addNum("Set Point", getSetPoint());
+        shuffleboard.addBoolean("At Point", atPoint());
+        shuffleboard.addNum("tolorance", this.tolorance);
+        shuffleboard.addBoolean("can move", canMove());
+      }
+    
+
 }
--- src/main/java/com/ma5951/utils/subsystem/InternallyControlledSubsystem.java
+++ src/main/java/com/ma5951/utils/subsystem/InternallyControlledSubsystem.java
@@ -1,6 +1,6 @@
 package com.ma5951.utils.subsystem;
 
-public interface InternallyControlledSubsystem extends ControllableSubsystem {
+public interface InternallyControlledSubsystem extends MotorSubsystem{
   public void calculate(double setPoint);
 
   public boolean atPoint();
